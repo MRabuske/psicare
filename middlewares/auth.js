7f07338a9111993c721752f64016d655255b9974
MRabuske <mbfrabuske@gmail.com>
2025-06-15 16:50:49 -0300

    Integração backend e front end

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,15 +10,12 @@ const auth = (req, res, next) => {
     }
 
     try {
-
         const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
         req.user = decoded
         next()
     } catch(err){
         return res.status(401).json({message: 'token invalido'})
     }
-
-    
 }
 
 export default auth
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import publicRoutes from './routes/public.js'
 import privateRoutes from './routes/private.js'
-
 import auth from './middlewares/auth.js'
+
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
 app.use('/', publicRoutes)
 app.use('/', auth, privateRoutes)
-
 
 app.listen(3000, () => console.log("Servidor rodando :)"))
 
